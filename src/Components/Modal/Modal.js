fece374b1a19cc7c362120bbbb24c0346cf24e6a
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-07 12:16:38 -0500

    created modal student details

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <div className="modal__left-col">
+        <img src="" />
+      </div>
+      <div className="modal__right-col"></div>
+    </div>
+  );
+};
+
+export default Modal;
