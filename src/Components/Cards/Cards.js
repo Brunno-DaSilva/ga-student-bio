7685803503ea3637e74083c21abfd7b3188b584a
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-07 16:35:38 -0500

    finished top modal styling portion

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal/Modal";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe, faPlus } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithubAlt,
   faLinkedinIn,
@@ -11,73 +12,85 @@ import {
 
 import "./cards-style.scss";
 
-const Cards = ({
-  name,
-  title,
-  imgUrl,
-  country,
-  city,
-  state,
-  aboutDescription,
-  linkedin,
-  portfolio,
-  github,
-}) => {
-  return (
-    <div className="cards-container">
-      <div className="cards__top">
-        <div className="cards__top--left">
-          <div className="name">
-            <p>Name: </p>
-            <p>{name}</p>
-          </div>
-          <div className="title">
-            <p>Title: </p>
-            <p>{title}</p>
-          </div>
-          <br />
-        </div>
-        <div className="cards__top--right">
-          <FontAwesomeIcon className="icon-plus" icon={faPlusCircle} />
-        </div>
-        <div className="image">
-          <img src={imgUrl} alt={title} />
-        </div>
-      </div>
-      <div className="cards__bottom">
-        <div className="cards__bottom--top">
-          <h2>About</h2>
-          <br />
-          <div>
-            <p>
-              Darth Maul was a Dathomirian Zabrak Sith Lord. In The Phantom
-              Menace, Sidious gave Maul his first mission: put the criminal
-              organization Black Sun in disarray
-            </p>
-          </div>
-        </div>
-        <div className="cards__bottom--bottom">
-          <div className="icons-container">
-            <div className="icons-bg">
-              <FontAwesomeIcon className="icons-social" icon={faGithubAlt} />
-            </div>
-            <div className="icons-bg">
-              <FontAwesomeIcon className="icons-social" icon={faTwitter} />
-            </div>
+const Cards = ({ name, title, imgUrl, ...otherSectionProps }) => {
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
-            <div className="icons-bg">
-              <FontAwesomeIcon className="icons-social" icon={faLinkedinIn} />
+  const modalIsOpenHandler = () => {
+    setModalIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div className="cards-container">
+        <div className="cards__top">
+          <div className="cards__top--left">
+            <div className="name">
+              <p>Name: </p>
+              <p>{name}</p>
             </div>
-            <div className="icons-bg">
-              <FontAwesomeIcon className="icons-social" icon={faGlobe} />
+            <div className="title">
+              <p>Title: </p>
+              <p>{title}</p>
             </div>
-            <div className="icons-bg">
-              <FontAwesomeIcon className="icons-social" icon={faInstagram} />
+            <br />
+          </div>
+          <div className="cards__top--right">
+            <div>
+              <button className="icon-plus-btn" onClick={modalIsOpenHandler}>
+                <FontAwesomeIcon className="icon-plus" icon={faPlus} />
+              </button>
+            </div>
+          </div>
+          <div className="image">
+            <img src={imgUrl} alt={title} />
+          </div>
+        </div>
+        <div className="cards__bottom">
+          <div className="cards__bottom--top">
+            <h2>About</h2>
+            <br />
+            <div>
+              <p>
+                Darth Maul was a Dathomirian Zabrak Sith Lord. In The Phantom
+                Menace, Sidious gave Maul his first mission: put the criminal
+                organization Black Sun in disarray
+              </p>
+            </div>
+          </div>
+          <div className="cards__bottom--bottom">
+            <div className="icons-container">
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faGithubAlt} />
+              </div>
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faTwitter} />
+              </div>
+
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faLinkedinIn} />
+              </div>
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faGlobe} />
+              </div>
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faInstagram} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {modalIsOpen ? (
+        <Modal
+          name={name}
+          title={title}
+          imgUrl={imgUrl}
+          {...otherSectionProps}
+        />
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,13 +1,90 @@
 import React from "react";
 
-const Modal = (props) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGlobe,
+  faGlobeAmericas,
+  faIdBadge,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithubAlt,
+  faLinkedinIn,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+import "./modal.scss";
+import Backdrop from "../Backdrop/Backdrop";
+
+const Modal = ({
+  title,
+  name,
+  imgUrl,
+  country,
+  state,
+  city,
+  aboutDescription,
+}) => {
   return (
-    <div className="modal">
-      <div className="modal__left-col">
-        <img src="" />
+    <>
+      <div className="modal">
+        <div className="modal__left-col">
+          <img src={imgUrl} />
+        </div>
+        <div className="modal__right-col">
+          <div className="right-col__name">
+            <p>{name}</p>
+          </div>
+          <div className="right-col__lo-ti">
+            <div className="right-col__title">
+              <div className="icons-bg">
+                <FontAwesomeIcon className="icons-social" icon={faIdBadge} />
+              </div>
+              <p>
+                <span>{title}</span>
+              </p>
+            </div>
+            <div className="right-col__location">
+              <div className="icons-bg">
+                <FontAwesomeIcon
+                  className="icons-social"
+                  icon={faGlobeAmericas}
+                />
+              </div>
+              <p>
+                <span>
+                  {city}, {state}, {country}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className="right-col__about">
+            <h3>About</h3>
+            <p>{aboutDescription}</p>
+          </div>
+          <div className="right-col__icons">
+            <div className="icons-bg">
+              <FontAwesomeIcon className="icons-social" icon={faGithubAlt} />
+            </div>
+            <div className="icons-bg">
+              <FontAwesomeIcon className="icons-social" icon={faTwitter} />
+            </div>
+
+            <div className="icons-bg">
+              <FontAwesomeIcon className="icons-social" icon={faLinkedinIn} />
+            </div>
+            <div className="icons-bg">
+              <FontAwesomeIcon className="icons-social" icon={faGlobe} />
+            </div>
+            <div className="icons-bg">
+              <FontAwesomeIcon className="icons-social" icon={faInstagram} />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal__right-col"></div>
-    </div>
+      <Backdrop />
+    </>
   );
 };
 
