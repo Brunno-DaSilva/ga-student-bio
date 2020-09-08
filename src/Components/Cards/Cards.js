74533e9e0627e4592084df590c91fa156dcf3bc2
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-08 15:57:51 -0500

    Added animation, created css/scssanimation and import to files

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -21,9 +21,9 @@ const Cards = ({ name, title, imgUrl, ...otherSectionProps }) => {
 
   return (
     <>
-      <div className="cards-container">
-        <div className="cards__top">
-          <div className="cards__top--left">
+      <div className="cards-container animated fadeInUp">
+        <div className="cards__top ">
+          <div className="cards__top--left animated delay-1 fadeInUp">
             <div className="name">
               <p>Name: </p>
               <p>{name}</p>
@@ -34,45 +34,45 @@ const Cards = ({ name, title, imgUrl, ...otherSectionProps }) => {
             </div>
             <br />
           </div>
-          <div className="cards__top--right">
+          <div className="cards__top--right animated delay-1 fadeInUp">
             <div>
               <button className="icon-plus-btn" onClick={modalIsOpenHandler}>
                 <FontAwesomeIcon className="icon-plus" icon={faPlus} />
               </button>
             </div>
           </div>
-          <div className="image">
+          <div className="image animated delay-4s fadeInUp">
             <img src={imgUrl} alt={title} />
           </div>
         </div>
         <div className="cards__bottom">
           <div className="cards__bottom--top">
-            <h2>About</h2>
+            <h2 className="animated delay-1s fadeInUp">About</h2>
             <br />
             <div>
-              <p>
+              <p className="animated delay-1shalf fadeInUp">
                 Darth Maul was a Dathomirian Zabrak Sith Lord. In The Phantom
                 Menace, Sidious gave Maul his first mission: put the criminal
                 organization Black Sun in disarray
               </p>
             </div>
           </div>
-          <div className="cards__bottom--bottom">
-            <div className="icons-container">
-              <div className="icons-bg">
-                <FontAwesomeIcon className="icons-social" icon={faGithubAlt} />
+          <div className="cards__bottom--bottom animated delay-1shalf fadeInUp">
+            <div className="icons-container ">
+              <div className="icons-bg animated delay-2s fadeInUp">
+                <FontAwesomeIcon className="icons-social " icon={faGithubAlt} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-2shalf fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faTwitter} />
               </div>
 
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-3s fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faLinkedinIn} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-3shalf fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faGlobe} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg icons-bg animated delay-4s fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faInstagram} />
               </div>
             </div>
--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -15,18 +15,18 @@ const Toolbar = (props) => {
             <SideDrawerButton click={props.drawerToggleClickHandler} />
           </div>
           <div className="toolbar__logo">
-            <Link to="/">
+            <Link className="animated delay-1shalf fadeInUp" to="/">
               <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1588899368/Logo/blogo-gray_doyoy3.png" />
             </Link>
           </div>
           <div className="spacer" />
           <div className="toolbar__nav-items">
             <div className="App__top-right">
-              <TopRight />
+              <TopRight className="animated delay-1 fadeInRight" />
             </div>
-            <Link>Map</Link>
-            <Link>Students</Link>
-            <Link>About</Link>
+            <Link className="animated delay-1shalf fadeInUp">Map</Link>
+            <Link className="animated delay-2s fadeInUp">Students</Link>
+            <Link className="animated delay-2shalf fadeInUp">About</Link>
           </div>
         </nav>
       </header>
