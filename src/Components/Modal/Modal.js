653ece07c9bbf416a48a298c4c5eb178472a153b
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-08 16:05:09 -0500

    Added animation to cards and modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -29,24 +29,24 @@ const Modal = ({
 }) => {
   return (
     <>
-      <div className="modal-container">
-        <div className="modal__close">
+      <div className="modal-container animated fadeInUp">
+        <div className="modal__close animated delay-1s fadeInUp">
           <button onClick={modalIsOpenHandler}>
             <div className="icons-bg">
               <FontAwesomeIcon className="icons-social" icon={faTimes} />
             </div>
           </button>
         </div>
-        <div className="modal">
-          <div className="modal__left-col">
+        <div className="modal animated delay-1s fadeInUp">
+          <div className="modal__left-col animated delay-1s fadeInUp">
             <img src={imgUrl} />
           </div>
           <div className="modal__right-col">
-            <div className="right-col__name">
+            <div className="right-col__name animated delay-1shalf fadeInUp">
               <h2>{name}</h2>
             </div>
-            <div className="right-col__lo-ti">
-              <div className="right-col__title">
+            <div className="right-col__lo-ti ">
+              <div className="right-col__title animated delay-2s fadeInUp">
                 <div className="icons-bg">
                   <FontAwesomeIcon className="icons-social" icon={faIdBadge} />
                 </div>
@@ -54,7 +54,7 @@ const Modal = ({
                   <span>{title}</span>
                 </p>
               </div>
-              <div className="right-col__location">
+              <div className="right-col__location animated delay-2s fadeInUp">
                 <div className="icons-bg">
                   <FontAwesomeIcon
                     className="icons-social"
@@ -69,25 +69,25 @@ const Modal = ({
               </div>
             </div>
 
-            <div className="right-col__about">
+            <div className="right-col__about animated delay-2s fadeInUp">
               <h3>About</h3>
               <p>{aboutDescription}</p>
             </div>
             <div className="right-col__icons">
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-2shalf fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faGithubAlt} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-3s fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faTwitter} />
               </div>
 
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-3shalf fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faLinkedinIn} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-4s fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faGlobe} />
               </div>
-              <div className="icons-bg">
+              <div className="icons-bg animated delay-4shalf fadeInUp">
                 <FontAwesomeIcon className="icons-social" icon={faInstagram} />
               </div>
             </div>
