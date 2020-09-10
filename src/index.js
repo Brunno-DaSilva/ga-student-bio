68d1139acae61ce2a2eea265eb6a570c057d876e
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-09 23:03:05 -0500

    before update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { ReactComponent as BottomLeft } from "./images/bottomleft.svg";
 import App from "./App";
 
 import "./index.css";
