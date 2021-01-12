7ec3d8a5c091c5d08645a5310205e3bd7776a29a
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-01-12 13:46:23 -0600

    added students BIOS in About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,15 @@ const About = () => {
       <div className="about__title">
         <h1>About</h1>
       </div>
+      <div className="about__body">
+        <div>
+          <h2>Studenst Bios</h2>
+          <p>
+            imple app built with create-react-app, so students can practice
+            contributing to open source.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
